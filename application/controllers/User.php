419e6b7a36475bc5e1beeabf805d44a64f025157
yzuniga <y.zunigam@hotmail.com>
2018-05-18 16:19:05 -0500

    Lista de recetas al inicio :v

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -2,105 +2,87 @@
 
 class User extends CI_Controller {
 
-public function __construct(){
+  public function __construct(){
+    parent::__construct();
+    $this->load->helper('url');
+    $this->load->model('user_model');
+    $this->load->library('session');
+  }
 
-        parent::__construct();
-  			$this->load->helper('url');
-  	 		$this->load->model('user_model');
-        $this->load->library('session');
+  public function index(){
+    //$res=$this->user_model->getRecipe();
+    //if($res){
+      //$data['result'] = $res;
+    $data=array(
+      'receta'=>$this->user_model->getRecipe()
+    );
+      $this->load->view('dashboard.php',$data);
+    //}
+    
+  }
 
-}
-
-public function index()
-{
-$this->load->view('dashboard.php');
-}
-
-public function register_user(){
-
-      $user=array(
+  public function register_user(){
+    $user=array(
       'name'=>$this->input->post('name'),
       'email'=>$this->input->post('email'),
       'password'=>sha1($this->input->post('password')),
+    );
+    print_r($user);
+    $email_check=$this->user_model->email_check($user['email']);
 
-        );
-        print_r($user);
+    if($email_check){
+      $this->user_model->register_user($user);
+      $this->session->set_flashdata('success_msg', 'Registered successfully.Now login to your account.');
+      redirect('user/login_view');
+    }else{
+      $this->session->set_flashdata('error_msg', 'Error occured,Try again.');
+      redirect('user');
+    }
+  }
 
-$email_check=$this->user_model->email_check($user['email']);
+  public function login_view(){
+    $this->load->view("login.php");
+  }
 
-if($email_check){
-  $this->user_model->register_user($user);
-  $this->session->set_flashdata('success_msg', 'Registered successfully.Now login to your account.');
-  redirect('user/login_view');
-
-}
-else{
-
-  $this->session->set_flashdata('error_msg', 'Error occured,Try again.');
-  redirect('user');
-
-
-}
-
-}
-
-public function login_view(){
-
-$this->load->view("login.php");
-
-}
-
-function login_user(){
-  
-  $user_login=array(
-
-  'email'=>$this->input->post('email'),
-  'password'=>sha1($this->input->post('password'))
-
+  function login_user(){    
+    $user_login=array(
+      'email'=>$this->input->post('email'),
+      'password'=>sha1($this->input->post('password'))
     );
 
     $data=$this->user_model->login_user($user_login['email'],$user_login['password']);
-      if($data)
-      {
-        $this->session->set_userdata('id',$data['id']);
-        $this->session->set_userdata('email',$data['email']);
-        $this->session->set_userdata('name',$data['name']);
- 
+    if($data){
+      $this->session->set_userdata('id',$data['id']);
+      $this->session->set_userdata('email',$data['email']);
+      $this->session->set_userdata('name',$data['name']);
+      $this->load->view("perfil.php");
+    }else{
+      $this->session->set_flashdata('error_msg', 'Error occured,Try again.');
+      $this->load->view("login.php");
+    }
+  }
 
-        $this->load->view("perfil.php");
+  function user_profile(){
+    $this->load->view('perfil.php');
+  }
 
-      }
-      else{
-        $this->session->set_flashdata('error_msg', 'Error occured,Try again.');
-        $this->load->view("login.php");
+  public function user_logout(){
+    $this->session->sess_destroy();
+    redirect('user/login_view', 'refresh');
+  }
 
-      }
-
-
-}
-
-function user_profile(){
-
-$this->load->view('perfil.php');
-
-}
-public function user_logout(){
-
-  $this->session->sess_destroy();
-  redirect('user/login_view', 'refresh');
-}
-
-public function viewauction()
-{
-
-
+  public function viewauction(){
     $data['query'] = $this->user_model->viewauction();   
     $this->load->view('etl.php', $data);
     $data['query'] = $this->user_model->viewauction();   
     $this->load->view('etl.php', $data);
-}
- 
+  }
 
+  /*public function list(){
+    $res=$this->user_model->getRecipe();
+    if($res){
+      $this->load->view()
+    }
+  }*/
 }
-
 ?>
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -1,46 +1,34 @@
 <?php
 class User_model extends CI_model{
-
-
-
-public function register_user($user){
-
-
-$this->db->insert('user', $user);
-
-}
-
-public function login_user($email,$password){
-
-  $this->db->select('*');
-  $this->db->from('user');
-  $this->db->where('email',$email);
-  $this->db->where('password',$password);
-
-  if($query=$this->db->get())
-  {
-      return $query->row_array();
-  }
-  else{
-    return false;
+  public function register_user($user){
+    $this->db->insert('user', $user);
   }
 
+  public function login_user($email,$password){
+    $this->db->select('*');
+    $this->db->from('user');
+    $this->db->where('email',$email);
+    $this->db->where('password',$password);
 
-}
-public function email_check($email){
-
-  $this->db->select('*');
-  $this->db->from('user');
-  $this->db->where('email',$email);
-  $query=$this->db->get();
-
-  if($query->num_rows()>0){
-    return false;
-  }else{
-    return true;
+    if($query=$this->db->get())    {
+        return $query->row_array();
+    } else{
+      return false;
+    }
   }
 
-}
+  public function email_check($email){
+    $this->db->select('*');
+    $this->db->from('user');
+    $this->db->where('email',$email);
+    $query=$this->db->get();
+
+    if($query->num_rows()>0){
+      return false;
+    }else{
+      return true;
+    }
+  }
 
   public function getCatalogosErrores(){
     $this->db->select('*');
@@ -54,14 +42,27 @@ public function email_check($email){
     }
   }
 
-  function viewauction()
-    {
-      $query =  $this->db->get('tbl_cuerpos_de_agua'); 
-      return $query->result() ;
-      $query1 =  $this->db->get('tbl_camellones'); 
-      return $query1->result() ;
-    }
+  /*function viewauction(){
+    $query =  $this->db->get('tbl_cuerpos_de_agua'); 
+    return $query->result() ;
+    $query1 =  $this->db->get('tbl_camellones'); 
+    return $query1->result() ;
+  }*/
+
+  public function getRecipe(){
+    $data=array();
+    $this->db->select('*');
+    $this->db->from('recipe');
+    $this->db->limit('5');
+    return $this->db->get()->result_array();
+    /*
+      obtiene la receta con su detalle de datos
+    $data=array();
+    $this->db->select('*');
+    $this->db->from('recipe');
+    $this->db->join('recipedetails','recipe.idRecipe = recipedetails.idRecipeD');
+    return $this->db->get()->result_array();*/
+
+  }
 }
-
-
 ?>
